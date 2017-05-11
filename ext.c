b18ce9a3a5cba9bd9bc3a85f96498c0deefaad6a
Daniel Guilford <dlguilford@gmail.com>
2017-05-11 12:37:19 -0400

    Minimize and clean up.

--- ext.c
+++ ext.c
@@ -3,38 +3,20 @@
 #include <stdlib.h>
 
 
-void usage(char* progname)
-{
-	printf("%s: <filename>\n", progname);
-	exit(EXIT_SUCCESS);
-}
-
-
-void print_extension(char* fname)
-{
-	char* prev = NULL;
-	char* next = strchr(fname, '.');
-
-	while (next != NULL) {
-		prev = next + 1;		/* eat the '.' */
-		next = strchr(prev, '.');
-	}
-
-	printf("%s\n", prev == NULL ? "<no extension>" : prev);
-}
-
-
-int main(int argc, char** argv)
+int 
+main(int argc, char** argv)
 {
 	int i;
 
 	if (argc == 1) {
-		usage(argv[0]);
+		printf("%s: [FILE]... \n", argv[0]);
+		exit(0);
 	}
 
 	for (i = 1; i < argc; ++i) {
-		print_extension(argv[i]);
+		char* prev = strrchr(argv[i], '.');
+		printf("%s\n", prev ? prev : "<no extension>" );
 	}
 	
-	return EXIT_SUCCESS;
+	return 0;
 }
